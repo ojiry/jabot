1e48d9224badb7a4f5acbbefb381415e0b80c832
Ryoji Yoshioka <ryoji.yoshioka@quipper.com>
2017-05-08 13:35:48 +0900

    Show repositories

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/google/go-github/github"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 )
 
 func main() {
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "... your access token ..."},
+		&oauth2.Token{AccessToken: accessToken},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 
@@ -16,6 +21,11 @@ func main() {
 
 	// list all repositories for the authenticated user
 	repos, _, err := client.Repositories.List(ctx, "", nil)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	fmt.Println(repos)
 }
